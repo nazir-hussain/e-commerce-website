d017fecfa03d5b11673b061a6cfaf3da083275f4
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-12-23 21:47:09 +0530

    Update Pages.jsx

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -9,6 +9,7 @@ import Annocument from "../components/annocument/Annocument"
 import Wrapper from "../components/wrapper/Wrapper"
 
 const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
+  
   return (
     <>
       <Home CartItem={CartItem} />
