560d1cdef413658e3a7fd1dc410cca34a4b9b00b
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-01-03 23:25:42 +0530

    Update Discount.jsx

--- src/components/discount/Discount.jsx
+++ src/components/discount/Discount.jsx
@@ -7,6 +7,7 @@ const Discount = () => {
       <section className='Discount background NewArrivals'>
         <div className='container'>
           <div className='heading d_flex'>
+    
             <div className='heading-left row  f_flex'>
               <img src='https://img.icons8.com/windows/32/fa314a/gift.png' />
               <h2>Big Discounts</h2>
